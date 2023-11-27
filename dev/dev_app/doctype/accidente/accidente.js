e121c9dc473c04d69d23f2401cba52f611b42a59
luisicho <luism18.1999@gmail.com>
2023-11-27 21:46:01 +0000

    Cambio Accidente JS

--- dev/dev_app/doctype/accidente/accidente.js
+++ dev/dev_app/doctype/accidente/accidente.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2023, Alejandro Hernandez and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("ACCIDENTE", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("ACCIDENTE", {
+	refresh(frm) {
+        frm.get_field("latitud_y_longitud").map.setView([21.485202, -104.877471],13);
+	},
+});
