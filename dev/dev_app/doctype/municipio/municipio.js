3acf22d47e907f48e18f06430b3a1773c61240de
luisicho <luism18.1999@gmail.com>
2023-11-30 20:50:48 +0000

    Creacion Doctype Localidad y Municipio

---
+++ dev/dev_app/doctype/municipio/municipio.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Alejandro Hernandez and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("MUNICIPIO", {
+// 	refresh(frm) {
+
+// 	},
+// });
