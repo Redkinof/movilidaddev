10b0d56aa2563c75e1c1ca15079d4c233e754863
Luisicho <luism18.1999@gmail.com>
2023-12-26 16:26:37 +0000

    ULTIMA ACTUALIZACION DICIEMBRE 2023

--- dev/dev_app/doctype/accidente/accidente.js
+++ dev/dev_app/doctype/accidente/accidente.js
@@ -1,8 +1,73 @@
 // Copyright (c) 2023, Alejandro Hernandez and contributors
 // For license information, please see license.txt
+var campos = [
+	'nombre',
+	'delegacion',
+	'nopatrulla',
+	'identificacion_agente',
+	'fecha_y_hora',
+	'municipio',
+	'localidad',
+	'libramiento_de_tepic',
+	'calle_y_no',
+	'colonia',
+	'entre_calle',
+	'peaton',
+	'bicicleta',
+	'autobus',
+	'combi',
+	'taxi',
+	'camion_de_carga',
+	'automovil_particular',
+	'motocicleta',
+	'vehiculos',
+	'peaton_o_ciclista',
+	'narrativa_de_los_hechos',
+	'tipo_de_accidente',
+	'otro_accidente',
+	'causa',
+	'otra_causa',
+	'clase',
+	'rh',
+	'delito',
+	'valoracion_de_danos',
+	'total_de_vehiculos_participantes',
+	'lesionados',
+	'fallecidos',
+	'hospitalizados',
+	'solo_danos',
+	'latitud_y_longitud',
+	'nombre_responsable',
+	'area',
+	'no_de_unidad',
+	'identificacion_respondiente',
+	'firmas_section',
+	'agente_de_movilidad',
+];
 
 frappe.ui.form.on("ACCIDENTE", {
+	// onload(frm){
+	// 	frm.toggle_enable(campos, frm.doc.workflow_state === 'Revision');
+	// 	if (frm.doc.workflow_state === 'Revision'){
+	// 		frm.set_df_property('comandante_en_turno', 'reqd', 1)
+	// 	}
+	// 	if (frm.doc.workflow_state === 'Espera Pago'){
+	// 		// frm.toggle_enable(campos, frm.doc.workflow_state === 'Espera Pago');
+	// 		frm.set_df_property('comandante_en_turno', 'read_only', 1)
+	// 	}
+	// 	// if (frm.doc.workflow_state === 'Pagado'){
+	// 	// 	frm.toggle_enable(campos, frm.doc.workflow_state === 'Pagado'); 
+	// 	// }
+	// },
 	refresh(frm) {
+		frm.toggle_enable(campos, frm.doc.workflow_state === 'Revision');
+		if (frm.doc.workflow_state === 'Revision'){
+			// frm.set_df_property('comandante_en_turno', 'reqd', 1)
+		}
+		if (frm.doc.workflow_state === 'Espera Pago'){
+			frm.set_df_property('comandante_en_turno', 'read_only', 1)
+		}
+
         frm.get_field("latitud_y_longitud").map.setView([21.485202, -104.877471],13);
 		frm.fields_dict.localidad.get_query = function(doc) {
 			return {
@@ -13,42 +78,52 @@ frappe.ui.form.on("ACCIDENTE", {
 		}
 		frappe.call({
 			method: "dev.dev_app.doctype.accidente.accidente.get_Contadores",
-			args: {papa: frm.doc.name},
+			args: {papa: frm.doc.name, dano:frm.doc.solo_danos},
 			freeze: false,
 			freeze_message: "Espera porfavor",
 			callback: function(r){
-				frm.set_value("lesionados",r.message[0] + "");
-				frm.set_value("hospitalizados",r.message[1] + "");
-				frm.set_value("fallecidos",r.message[2] + "");
+				if(frm.doc.solo_danos != 1){
+					frm.set_value("lesionados",r.message[0] + "");
+					frm.set_value("hospitalizados",r.message[1] + "");
+					frm.set_value("fallecidos",r.message[2] + "");
+				}else{
+					frm.set_value("lesionados","0");
+					frm.set_value("hospitalizados","0");
+					frm.set_value("fallecidos","0");
+				}
+				frm.set_value("total_de_vehiculos_participantes",r.message[3] + "");
 			}
 		});
 	},
 	before_save(frm){
 		frappe.call({
 			method: "dev.dev_app.doctype.accidente.accidente.get_Contadores",
-			args: {papa: frm.doc.name},
+			args: {papa: frm.doc.name, dano:frm.doc.solo_danos},
 			freeze: false,
 			freeze_message: "Espera porfavor",
 			callback: function(r){
-				frm.set_value("lesionados",r.message[0] + "");
-				frm.set_value("hospitalizados",r.message[1] + "");
-				frm.set_value("fallecidos",r.message[2] + "");
+				if(frm.doc.solo_danos != 1){
+					frm.set_value("lesionados",r.message[0] + "");
+					frm.set_value("hospitalizados",r.message[1] + "");
+					frm.set_value("fallecidos",r.message[2] + "");
+				}else{
+					frm.set_value("lesionados","0");
+					frm.set_value("hospitalizados","0");
+					frm.set_value("fallecidos","0");
+				}
 				frm.set_value("total_de_vehiculos_participantes",r.message[3] + "");
 			}
 		});
 	},
-	after_save(frm){
-		frappe.call({
-			method: "dev.dev_app.doctype.accidente.accidente.get_Contadores",
-			args: {papa: frm.doc.name},
-			freeze: false,
-			freeze_message: "Espera porfavor",
-			callback: function(r){
-				frm.set_value("lesionados",r.message[0] + "");
-				frm.set_value("hospitalizados",r.message[1] + "");
-				frm.set_value("fallecidos",r.message[2] + "");
-				frm.set_value("total_de_vehiculos_participantes",r.message[3] + "");
-			}
-		});
+	solo_danos(frm){
+		if(frm.doc.solo_danos == 1){
+			frm.set_value("lesionados","0");
+			frm.set_value("hospitalizados","0");
+			frm.set_value("fallecidos","0");
+		}
+	},
+	cal(){
+
 	}
+
 });
--- dev/dev_app/doctype/accidente/accidente.py
+++ dev/dev_app/doctype/accidente/accidente.py
@@ -9,9 +9,11 @@ class ACCIDENTE(Document):
 	pass
 
 @frappe.whitelist()
-def get_Contadores(papa):
+def get_Contadores(papa, dano):
+	vehiculos = frappe.db.count('VEHICULOS ACCIDENTE', {'parent': papa})
 	lesionados = frappe.db.count('PEATON O CICLISTA', {'lesionado': '1', 'parent': papa})
 	hospitalizados = frappe.db.count('PEATON O CICLISTA', {'hospitalizacion': '1', 'parent': papa})
 	fallecidos = frappe.db.count('PEATON O CICLISTA', {'fallecido': '1', 'parent': papa})
-	vehiculos = frappe.db.count('VEHICULOS ACCIDENTE', {'parent': papa})
+	if dano == 1:
+		lesionados = hospitalizados = fallecidos = 0	
 	return [lesionados,hospitalizados,fallecidos, vehiculos]
