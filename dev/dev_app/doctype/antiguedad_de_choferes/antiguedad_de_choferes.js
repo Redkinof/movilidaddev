cd45bbf86d319eb28a50c3517770a7772f9a3fd1
Debian <admin@ip-172-31-83-83>
2024-02-12 16:09:09 +0000

    Se creo formato de choferes

--- dev/dev_app/doctype/antiguedad_de_choferes/antiguedad_de_choferes.js
+++ dev/dev_app/doctype/antiguedad_de_choferes/antiguedad_de_choferes.js
@@ -7,5 +7,11 @@ frappe.ui.form.on("ANTIGUEDAD DE CHOFERES", {
         var date = nowDate.getFullYear()+'/'+(nowDate.getMonth()+1)+'/'+nowDate.getDate();
         frm.set_value("fecha_de_registro",date);
         frm.set_df_property('fecha_de_registro', 'read_only', 1)
-   }
+   },
+   fecha_inicio_de_registro(frm){
+        var noD = new Date();
+        var newyear = noD.getFullYear() - frm.fecha_inicio_de_registro.getFullYear();
+        frm.set_value("ano_antiguedad",newyear);
+        frm.set_df_property('ano_antiguedad', 'read_only', 1)
+    }
 });
