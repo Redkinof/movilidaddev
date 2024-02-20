e37c0c7cbb7f2f21dc816414783c08b6323093f1
luisicho <luism18.1999@gmail.com>
2024-02-20 21:25:41 +0000

    Dias de Antiguedad

--- dev/dev_app/doctype/antiguedad_de_choferes/antiguedad_de_choferes.js
+++ dev/dev_app/doctype/antiguedad_de_choferes/antiguedad_de_choferes.js
@@ -3,15 +3,16 @@
 
 frappe.ui.form.on("ANTIGUEDAD DE CHOFERES", {
    refresh(frm) {
-        var nowDate = new Date(); 
-        var date = nowDate.getFullYear()+'/'+(nowDate.getMonth()+1)+'/'+nowDate.getDate();
-        frm.set_value("fecha_de_registro",date);
-        frm.set_df_property('fecha_de_registro', 'read_only', 1)
+     //    var nowDate = new Date(); 
+     //    var date = nowDate.getFullYear()+'/'+(nowDate.getMonth()+1)+'/'+nowDate.getDate();
+     //    frm.set_value("fecha_de_registro",date);
+     //    frm.set_df_property('fecha_de_registro', 'read_only', 1)
    },
    fecha_inicio_de_registro(frm){
         var noD = new Date();
-        var newyear = noD.getFullYear() - frm.fecha_inicio_de_registro.getFullYear();
-        frm.set_value("ano_antiguedad",newyear);
-        frm.set_df_property('ano_antiguedad', 'read_only', 1)
+     //    var newyear = noD.getFullYear() - frm.doc.fecha_inicio_de_registro.getFullYear();
+        var newyear = frappe.datetime.get_diff(noD, frm.doc.fecha_inicio_de_registro);
+        frm.set_value("dias_de_antiguedad",newyear);
+        frm.set_df_property('dias_de_antiguedad', 'read_only', 1)
     }
 });
