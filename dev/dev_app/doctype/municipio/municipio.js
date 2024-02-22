45cab833c1b700d9ec03911067c8859c8d85888d
luisicho <luism18.1999@gmail.com>
2024-02-22 22:51:08 +0000

    Se elimino Doctype Municipio, integrando con SEMOVINAY

--- dev/dev_app/doctype/municipio/municipio.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, Alejandro Hernandez and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("MUNICIPIO", {
-// 	refresh(frm) {
-
-// 	},
-// });
