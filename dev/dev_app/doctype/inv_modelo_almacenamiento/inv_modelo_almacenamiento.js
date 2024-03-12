7dd7936c91c846a008304eff3c0f837ef5f687a3
luisicho <luism18.1999@gmail.com>
2024-03-12 17:05:01 +0000

    doctype de inventario

---
+++ dev/dev_app/doctype/inv_modelo_almacenamiento/inv_modelo_almacenamiento.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Alejandro Hernandez and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Inv_Modelo_Almacenamiento", {
+// 	refresh(frm) {
+
+// 	},
+// });
